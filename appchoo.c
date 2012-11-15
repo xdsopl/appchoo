783b343315169f78cb8c1f8c96c389aec8116104
Ahmet Inan <ainan@mathematik.uni-freiburg.de>
2012-11-15 22:18:24 +0100

    scale image to fit window

--- appchoo.c
+++ appchoo.c
@@ -8,6 +8,36 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
+int fit_image(SDL_Surface *image, int w, int h)
+{
+	if (!image)
+		return 0;
+	if (image->format->BytesPerPixel != 3)
+		return 0;
+	if (w >= image->w && h >= image->h)
+		return 1;
+	int f = 0;
+	do f++; while (image->w / f > w || image->h / f > h);
+	w = image->w;
+	h = image->h;
+	image->clip_rect.w = image->w /= f;
+	image->clip_rect.h = image->h /= f;
+	image->pitch = image->w * 3;
+	uint8_t *pixels = image->pixels;
+	for (int y = 0; y < image->h; y++) {
+		for (int x = 0; x < image->w; x++) {
+			uint32_t s[3] = {0, 0, 0};
+			for (int j = 0; j < f; j++)
+				for (int i = 0; i < f; i++)
+					for (int c = 0; c < 3; c++)
+						 s[c] += pixels[((y*f + j) * w + x*f + i) * 3 + c];
+			for (int c = 0; c < 3; c++)
+				pixels[(y * image->w + x) * 3 + c] = s[c] / (f*f);
+		}
+	}
+	return 1;
+}
+
 void handle_events()
 {
 	SDL_Event event;
@@ -68,9 +98,7 @@ int main(int argc, char **argv)
 	SDL_WM_SetCaption("Application Chooser", "appchoo");
 
 	SDL_Surface *image = IMG_Load(image_name);
-	if (!image)
-		exit(1);
-	if (image->format->BytesPerPixel != 3)
+	if (!fit_image(image, screen->w, screen->h))
 		exit(1);
 
 	SDL_Rect src, dest;
