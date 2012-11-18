848141896d98d2bbe9a09df65e401360a27d27bd
Ahmet Inan <ainan@mathematik.uni-freiburg.de>
2012-11-18 18:12:06 +0100

    changed license to CC0

--- appchoo.c
+++ appchoo.c
@@ -1,8 +1,9 @@
-
-/* appchoo - Application Chooser with Timeout
- * (c) 2012 Ahmet Inan <ainan@mathematik.uni-freiburg.de>
- * written from scratch, public domain
- */
+/*
+appchoo - Application Chooser with Timeout
+Written in 2012 by <Ahmet Inan> <ainan@mathematik.uni-freiburg.de>
+To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+*/
 
 #include <math.h>
 #include <SDL.h>
